fe472c2662602a81ef5319e843e5032b019d12e0
Sam Roberts <vieuxtech@gmail.com>
2013-08-29 15:46:00 -0700

    Use consistent capitalization in comments

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ var loopback = require('loopback')
   , request = require('request')
   , TaskEmitter = require('strong-task-emitter');
 
-// expose a rest api
+// Expose a rest api
 app.use(loopback.rest());
 
 // Add static files
 app.use(loopback.static(path.join(__dirname, 'public')));
 
-// require models
+// Require models
 fs
   .readdirSync(path.join(__dirname, './models'))
   .filter(function (m) {
@@ -26,8 +26,8 @@ fs
     app.model(require('./models/' + m));
   });
   
-// enable docs
+// Enable docs
 app.docs({basePath: 'http://localhost:3000'});
 
-// start the server
+// Start the server
 app.listen(3000);
