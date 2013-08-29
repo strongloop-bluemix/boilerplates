9a5734511ca3df502204893f0b90b37b584a9c7c
Sam Roberts <vieuxtech@gmail.com>
2013-08-29 16:17:43 -0700

    Use cluster control if configured for clustering

--- app.js
+++ app.js
@@ -2,6 +2,14 @@
  * App Dependencies.
  */
 
+var control = require('strong-cluster-control');
+var options = control.loadOptions();
+
+// If configured as a cluster master, just start controller
+if(options.clustered && options.isMaster) {
+  return control.start(options);
+}
+
 var loopback = require('loopback')
   , app = module.exports = loopback()
   , fs = require('fs')
