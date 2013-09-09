51ad23db575f47f85879d3207dea5e1f57a2017d
Raymond Feng <raymond@strongloop.com>
2013-09-09 09:06:51 -0700

    Change the env var to PARTNER

--- build/readme.js
+++ build/readme.js
@@ -1,22 +1,24 @@
 /**
- * The utility sets up the default README.md based on the PAAS_PROVIDER environment variable
+ * The utility sets up the default README.md based on the PARTNER environment variable
+ *
+ * For example `PARTNER=cloud9 npm install` will replace README.md with README.cloud9.md
  */
 var fs = require('fs');
 var path = require('path');
 
-if (process.env.PAAS_PROVIDER) {
+if (process.env.PARTNER) {
   var readme = path.join(__dirname, '../README.md'); // README.md
   var slReadme = path.join(__dirname, '../README.strongloop.md'); // README.strongloop.md
-  var paasReadme = path.join(__dirname, '../README.' + process.env.PAAS_PROVIDER + '.md'); // README.<paas_provider>.md, for example README.cloud9.md
+  var partnerReadme = path.join(__dirname, '../README.' + process.env.PARTNER + '.md'); // README.<partner>.md, for example README.cloud9.md
   // If the provider specific README exists
-  if (fs.existsSync(paasReadme)) {
+  if (fs.existsSync(partnerReadme)) {
     // Rename README.md to be README.strongloop.md
     if (fs.existsSync(readme)) {
       fs.renameSync(readme, slReadme);
       console.log(readme, '-->', slReadme);
     }
-    // Rename README.<paas_provider>.md to be README.md
-    fs.renameSync(paasReadme, readme);
-    console.log(paasReadme, '-->', readme);
+    // Rename README.<partner>.md to be README.md
+    fs.renameSync(partnerReadme, readme);
+    console.log(partnerReadme, '-->', readme);
   }
 }
