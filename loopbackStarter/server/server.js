29dda6369d99d90371b2530228418798a0cf9dfe
Sai Vennam <svennam@us.ibm.com>
2015-10-14 19:25:20 -0400

    update loopback starter

--- loopbackStarter/server/server.js
+++ loopbackStarter/server/server.js
@@ -3,38 +3,19 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
-// Set up the /favicon.ico
-app.use(loopback.favicon());
-
-// request pre-processing middleware
-app.use(loopback.compress());
-
-// -- Add your pre-processing middleware here --
-
 // boot scripts mount components like REST API
 boot(app, __dirname);
 
-// -- Mount static files here--
-// All static middleware should be registered at the end, as all requests
-// passing the static middleware are hitting the file system
-// Example:
-//   app.use(loopback.static(path.resolve(__dirname', '../client')));
-var websitePath = require('path').resolve(__dirname, '../client');
-app.use(loopback.static(websitePath));
-
-// Requests that get this far won't be handled
-// by any middleware. Convert them into a 404 error
-// that will be handled later down the chain.
-app.use(loopback.urlNotFound());
-
-// The ultimate error handler.
-app.use(loopback.errorHandler());
-
 app.start = function() {
   // start the web server
-  return app.listen(function() {
+  return app.listen(process.env.PORT || 3000, function() {
     app.emit('started');
-    console.log('Web server listening at: %s', app.get('url'));
+    var baseUrl = app.get('url').replace(/\/$/, '');
+    console.log('Web server listening at: %s', baseUrl);
+    if (app.get('loopback-component-explorer')) {
+      var explorerPath = app.get('loopback-component-explorer').mountPath;
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+    }
   });
 };
 
